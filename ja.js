039d8c002668669912a380b5f0207e48db8a8fea
rocnail23 <jdanielrojas16@gmail.com>
2023-01-06 08:43:11 -0400

    last commit

--- ja.js
+++ ja.js
@@ -1,6 +1,17 @@
 const btn = document.querySelector(".btn")
 const nav = document.querySelector(".container-menu")
+const btni = document.querySelectorAll(".btn-li")
 
  btn.addEventListener('click', function(){
     nav.classList.toggle('container-add')
+    
 })
+
+btni.forEach(function(li){
+    li.addEventListener("click",function(){
+        nav.classList.toggle('container-add')
+    })
+})
+
+
+
